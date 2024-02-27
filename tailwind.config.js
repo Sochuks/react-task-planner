016597b881389b0392c5b33533c23029ed74a124
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-27 17:51:46 +0100

    Modified and set general styles for tailwind & project CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    // Screen Sizes
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      // Portfolio Color Scheme
+      colors:{
+        bgColor: '#1f1f38',
+        bg2Color: '#2c2c6c',
+        primaryColor: '4db5ff',
+        primary2Color: 'rgb(77, 181, 125, 0.4)',
+        whiteColor: '#fff',
+        lightColor: 'rgb(255, 255, 255, 0.6)',
+      },
+    },
   },
   plugins: [],
 }
