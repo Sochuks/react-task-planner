e950e603e252f95c149229fd4c7f0fd2e7417967
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-27 19:26:24 +0100

    Created Button Component and add to App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Button from './Button'
 
 const Header = () => {
   return (
 
     <header className='flex justify-between items-center mb-8'>
         <h1>Task Planner</h1>
+        < Button />
     </header>
     
   )
