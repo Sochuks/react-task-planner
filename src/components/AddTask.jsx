7c6c36c0106edc2e387c5538d1accccbe0ae96d7
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 16:07:08 +0100

    Add form elements

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,6 +1,24 @@
 const AddTask = () => {
   return (
-    <div>AddTask</div>
+    // Add Task Form 
+    <form>
+        <div className="form-control">
+            <label>Task</label>
+            <input type="text" placeholder="Add Task" />
+        </div>
+
+        <div className="form-control">
+            <label>Day</label>
+            <input type="text" placeholder="Add Day & time" />
+        </div>
+
+        <div className="form-control">
+            <label>Set Reminder</label>
+            <input type="checkbox" />
+        </div>
+
+        <input type="submit" value="Save Task" />
+    </form>
   )
 }
 
