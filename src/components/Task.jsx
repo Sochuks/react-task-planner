69a32347bc7f83d9fd524df0018fba1c37e414c3
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 15:42:09 +0100

    Added a condition to set left border if task has a reminder.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,7 @@ import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task, onDelete, onToggle}) => {
   return (
-    <div className="m-2 px-3 py-5 cursor-pointer bg-gray-100 rounded-md"
+    <div className={`m-2 px-3 py-5 cursor-pointer bg-gray-100 rounded-md ${task.reminder ? 'border-l-4 border-green-600' : ''}`}
     onDoubleClick={() => onToggle(task.id)}>
             <h3 className="flex items-center justify-between font-semibold">{task.text} 
             <FaTimes style={{color:'red', cursor:'pointer'}} onClick={()=> onDelete(task.id) } /></h3>
