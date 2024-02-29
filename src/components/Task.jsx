6a435ac3c1eb1fc0f864534b22c3bf5fd3fc76c1
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 09:55:47 +0100

    Created a onClick event for delete icon and set to call onDelete function

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,10 @@
 import {FaTimes} from 'react-icons/fa'
 
-const Task = ({task}) => {
+const Task = ({task, onDelete}) => {
   return (
     <div className="m-2 px-3 py-5 cursor-pointer bg-gray-100 rounded-md">
             <h3 className="flex items-center justify-between font-semibold">{task.text} 
-            <FaTimes style={{color:'red', cursor:'pointer'}} /></h3>
+            <FaTimes style={{color:'red', cursor:'pointer'}} onClick={()=> onDelete(task.id) } /></h3>
             <p>{task.day}</p>
     </div>
   )
