302e693fb0c22f4c8275d1ed7acfbefb6adb77b9
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 17:11:12 +0100

    Modified style for form and form elements using css and tailwind

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,23 +1,23 @@
 const AddTask = () => {
   return (
     // Add Task Form 
-    <form>
+    <form className="mb-10">
         <div className="form-control">
             <label>Task</label>
-            <input type="text" placeholder="Add Task" />
+            <input type="text" placeholder="Add Task" className="h-10" />
         </div>
 
         <div className="form-control">
             <label>Day</label>
-            <input type="text" placeholder="Add Day & time" />
+            <input type="text" placeholder="Add Day & time" className="h-10" />
         </div>
 
-        <div className="form-control">
-            <label>Set Reminder</label>
-            <input type="checkbox" />
+        <div className="form-control flex items-center justify-between">
+            <label className="flex-1">Set Reminder</label>
+            <input type="checkbox" className="flex-2 h-5" />
         </div>
 
-        <input type="submit" value="Save Task" />
+        <input className="w-full mx-auto btn mt-5" type="submit" value="Save Task" />
     </form>
   )
 }
