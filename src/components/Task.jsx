9aa1d034edaf361936c44a02a4a197193a798b97
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 09:30:53 +0100

    Using react icons as a component to add delete icon to task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,10 @@
+import {FaTimes} from 'react-icons/fa'
+
 const Task = ({task}) => {
   return (
-    <div className="m-2 px-3 py-5 cursor-pointer bg-gray-100">
-        <h3 className="flex items-center justify-between">{task.text}</h3>
+    <div className="m-2 px-3 py-5 cursor-pointer bg-gray-100 rounded-md">
+            <h3 className="flex items-center justify-between font-semibold">{task.text} <FaTimes /></h3>
+            <p>{task.day}</p>
     </div>
   )
 }
