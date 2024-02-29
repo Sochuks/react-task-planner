d2ddf3949a14fe3aa01b13a4810491f8d48d9338
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 09:32:50 +0100

    add inline-style to delete icon

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,8 @@ import {FaTimes} from 'react-icons/fa'
 const Task = ({task}) => {
   return (
     <div className="m-2 px-3 py-5 cursor-pointer bg-gray-100 rounded-md">
-            <h3 className="flex items-center justify-between font-semibold">{task.text} <FaTimes /></h3>
+            <h3 className="flex items-center justify-between font-semibold">{task.text} 
+            <FaTimes style={{color:'red', cursor:'pointer'}} /></h3>
             <p>{task.day}</p>
     </div>
   )
