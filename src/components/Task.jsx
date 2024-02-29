0c1d8bbc3bb998293cb3886c045ba30780fc3e46
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 09:15:47 +0100

    Modify style for task & task header

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,7 @@
 const Task = ({task}) => {
   return (
-    <div >
-        <h3>{task.text}</h3>
+    <div className="m-2 px-3 py-5 cursor-pointer bg-gray-100">
+        <h3 className="flex items-center justify-between">{task.text}</h3>
     </div>
   )
 }
