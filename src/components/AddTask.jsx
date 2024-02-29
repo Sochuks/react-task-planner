df8b955869fa2e2a31d06d65690d083da18a7cfb
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-29 18:24:12 +0100

    Create event for input elements and setState to current inputed values

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -11,17 +11,20 @@ const AddTask = () => {
     <form className="mb-10">
         <div className="form-control">
             <label>Task</label>
-            <input type="text" placeholder="Add Task" className="h-10" value={text} />
+            <input type="text" placeholder="Add Task" className="h-10" value={text} 
+            onChange={(e) => setText(e.target.value)} />
         </div>
 
         <div className="form-control">
             <label>Day</label>
-            <input type="text" placeholder="Add Day & time" className="h-10" value={day} />
-        </div>
+            <input type="text" placeholder="Add Day & time" className="h-10" value={day} 
+            onChange={(e)=> setDay(e.target.value)}/>
+         </div>
 
         <div className="form-control flex items-center justify-between">
             <label className="flex-1">Set Reminder</label>
-            <input type="checkbox" className="flex-2 h-5" value={reminder} />
+            <input type="checkbox" className="flex-2 h-5" value={reminder} 
+            onChange={(e)=> setReminder(e.currentTarget.checked)}/>
         </div>
 
         <input className="w-full mx-auto btn mt-5" type="submit" value="Save Task" />
