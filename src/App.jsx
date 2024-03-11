ab3a3d33ec9e2c0d32bb9a5b497574b2dc38c5f2
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-11 18:37:12 +0100

    import router and routes from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import Header from './components/Header'
 import Footer from './components/Footer'
