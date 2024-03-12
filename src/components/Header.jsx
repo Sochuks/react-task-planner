b381b58ab8e74ac6f5730f8e6b7c38040c714f5b
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-12 09:47:59 +0100

    import useLocation to make add button visible on homepage on

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
-import {Link} from 'react-router-dom'
+import {useLocation} from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({showTaskForm, showAdd}) => {
-  return (
-    
+  const location = useLocation()
 
-    <header className='flex justify-between items-center mb-8'>
-        <h1>Task Planner</h1>
-        <Button color={showAdd ? 'red' : 'green'} text ={showAdd ? 'Close' : 'Add'} onClick={showTaskForm} />
-    </header>
-    
-  )
+  
+    return (
+
+      <header className='flex justify-between items-center mb-8'>
+          <h1>Task Planner</h1>
+         {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text ={showAdd ? 'Close' : 'Add'} onClick={showTaskForm} />}
+      </header>
+      
+    )
 }
 
 export default Header
